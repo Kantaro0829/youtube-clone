c2d712e5d03780ebd77e76edc912cee43c346389
Kantaro0829 <eshangwan@gmail.com>
2022-09-14 19:54:25 +0900

    [add] express & mongo の環境構築

---
+++ mongo/1-mongo-init.js
@@ -0,0 +1,17 @@
+
+var users = [
+    {
+      user: "test",
+      pwd: "test",
+      roles: [
+        {
+          role: "dbOwner",
+          db: "test"
+        }
+      ]
+    }
+  ];
+  
+  for (var i = 0, length = users.length; i < length; ++i) {
+    db.createUser(users[i]);
+  }
