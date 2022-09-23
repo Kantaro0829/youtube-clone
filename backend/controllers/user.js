6339816a9826e86b1297804b0e124089ec684a5d
Kantaro0829 <eshangwan@gmail.com>
2022-09-23 10:05:57 +0900

    [update] JWT実装完了

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,90 @@
+import { createError } from "../error.js"
+import User from "../models/User.js"
+
+
 export const test = (req, res)=>{
     res.json("it's successful")
     console.log("test function is working!!!!")
+}
+
+
+export const update = async (req, res, next) => {
+    if(req.params.id === req.user.id) {
+
+        try{
+            const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body
+            },
+            { new:true }
+            )
+            res.status(200).json(updatedUser)
+        }catch(err){
+            next(err)
+        }
+
+    }else{
+        return next(createError("403", "You can update only your account!"))
+    }
+}
+
+
+export const deleteUser = async (req, res, next) => {
+    if(req.params.id === req.user.id) {
+  
+        try{
+            await User.findByIdAndDelete(
+                req.params.id
+            )
+            res.status(200).json("User has been deleted!")
+        }catch(err){
+            next(err)
+        }
+
+    }else{
+        return next(createError("403", "You can delete only your account!"))
+    }
+}
+
+
+export const getUser = async(req, res, next) => {
+    try{
+        //todo
+
+    }catch(err){
+        next(err)
+    }
+    
+}
+
+export const subscribe = async(req, res, next) => {
+    try{
+        //todo
+        
+    }catch(err){
+        next(err)
+    }
+}
+export const unsubscribe = async(req, res, next) => {
+    try{
+        //todo
+        
+    }catch(err){
+        next(err)
+    }
+}
+export const like = async(req, res, next) => {
+    try{
+        //todo
+        
+    }catch(err){
+        next(err)
+    }
+}
+export const dislike = async(req, res, next) => {
+    try{
+        //todo
+        
+    }catch(err){
+        next(err)
+    }
 }
